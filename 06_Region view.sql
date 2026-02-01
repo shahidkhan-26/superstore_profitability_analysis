193d809aa0335734754570c8adc1cd38b18de9fb
shahidkhan-26 <shahidkhansalfi025@gmail.com>
2026-02-01 22:56:24 +0530

    Delete 06_Region view.sql

--- 06_Region view.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE VIEW `superstore-sales-484717.sales.v_region` AS
-  SELECT DISTINCT
-    FARM_FINGERPRINT(CONCAT(Country, '|', Region))
-  AS `Region ID`,
-    Country,
-    Region,
-    State
-FROM `superstore-sales-484717.sales.orders`
