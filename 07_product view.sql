54ebe7f661820ca9f6d1f173674c274208db886e
shahidkhan-26 <shahidkhansalfi025@gmail.com>
2026-02-01 22:56:37 +0530

    Delete 07_product view.sql

--- 07_product view.sql
+++
@@ -1,7 +0,0 @@
-CREATE VIEW `superstore-sales-484717.sales.products` AS
-  SELECT DISTINCT
-    `Product ID`,
-    `Product Name`,
-    `Category`,
-    `Sub-Category`
-FROM `superstore-sales-484717.sales.orders`
